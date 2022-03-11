b1edf3e55b028b639df0e2b492dea05d04c0fc3b
ericaohashi <m.0084erica.m.july@gmail.com>
2022-03-11 15:31:05 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname,presence:true
+         validates :last_name,format:{with:/\A[ぁ-んァ-ン一-龥]/},presence:true # 全角ひらがな、全角カタカナ、漢字のみ
+         validates :first_name,format:{with:/\A[ぁ-んァ-ン一-龥]/},presence:true # 全角ひらがな、全角カタカナ、漢字のみ
+         validates :last_kana,format:{with:/\A[ァ-ヶー－]+\z/},presence:true # 全角カタカナ
+         validates :first_kana,format:{with:/\A[ァ-ヶー－]+\z/},presence:true # 全角カタカナ
+         validates :password,presence:true,length: {minimum: 6 },format:{ with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+         validates :birth,presence:true
+         
 end
--- db/migrate/20220308080851_devise_create_users.rb
+++ db/migrate/20220308080851_devise_create_users.rb
@@ -3,7 +3,14 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
+      t.string :last_name,          null:false
+      t.string :first_name,         null:false
+      t.string :last_kana,          null:false
+      t.string :first_kana,         null:false
+      t.string :nickname,           null:false
+      t.date   :birth,              null:false
       ## Database authenticatable
+      
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
