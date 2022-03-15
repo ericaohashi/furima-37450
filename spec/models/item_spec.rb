c6684d70f28e919ad5187c1a5a251340b4ff472e
ericaohashi <m.0084erica.m.july@gmail.com>
2022-03-15 21:05:45 +0900

    商品出品の修正３

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -76,6 +76,11 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not a number")
       end
+      it '金額が空では登録できない'do
+      @item.price = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price can't be blank")
+    end
     end
   end
 end
