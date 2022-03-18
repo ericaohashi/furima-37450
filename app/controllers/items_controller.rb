517edad9e7e363c45470aa637cd4dffcc217b740
ericaohashi <m.0084erica.m.july@gmail.com>
2022-03-18 14:24:43 +0900

    商品削除の修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,8 +40,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    unless current_user.id == @item.user_id
     @item.destroy
     redirect_to root_path
+    end
   end
 
 
