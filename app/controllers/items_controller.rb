7637bb9e5dcc062136dee52bd91da80ced7a9da3
ericaohashi <m.0084erica.m.july@gmail.com>
2022-03-18 13:48:25 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only:[:new, :create,:edit]
-  before_action :find_params,only:[:show, :edit, :update]
+  before_action :authenticate_user!,only:[:new, :create,:edit,:destroy]
+  before_action :find_params,only:[:show, :edit, :update, :destroy]
   def index
     
     @items = Item.order("created_at DESC")
@@ -39,6 +39,13 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+
+  
   private
 
   def item_params
